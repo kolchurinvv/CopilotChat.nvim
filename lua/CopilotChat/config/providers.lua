5517e10843b40775d468c2a7a8ce13427b66e338
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2026-04-01 17:21:50 +0200

    providers - no top_p n and temperature for -codex gpt models

--- lua/CopilotChat/config/providers.lua
+++ lua/CopilotChat/config/providers.lua
@@ -278,6 +278,8 @@ end
 ---@return table
 local function prepare_chat_input(inputs, opts)
   local is_o1 = vim.startswith(opts.model.id, 'o1')
+  local regex = vim.regex('codex')
+  local is_codex = regex.match_str(regex, opts.model.id) ~= nil
 
   inputs = vim.tbl_map(function(input)
     local output = {
@@ -324,7 +326,7 @@ local function prepare_chat_input(inputs, opts)
     end, opts.tools)
   end
 
-  if not is_o1 then
+  if not is_o1 and not is_codex then
     out.n = 1
     out.top_p = 1
     out.temperature = opts.temperature
